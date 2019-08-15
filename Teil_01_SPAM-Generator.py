1794d0962f71a20e19e206abb940fb2529f1665a
Your Name <you@example.com>
2019-08-15 20:53:49 +0200

    teständerung

--- Teil_01_SPAM-Generator.py
+++ Teil_01_SPAM-Generator.py
@@ -1,4 +1,5 @@
 from random import shuffle
+import matplotlib
 
 superlatives = "amazing gorgeous blazing stunning tremendous greatest best fantastic \
                phenomenal delightful ambitious outstanding incredible spectacular \
